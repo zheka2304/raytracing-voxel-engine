45d50a6f72625b19a92aa883f1c0b61106e6a87e
Eugene Smirnov <zheka2304@gmail.com>
2021-06-17 23:59:34 +0300

    (not tested) more opengl entities wrapped in classes

--- source/voxel/common/opengl.cc
+++ source/voxel/common/opengl.cc
@@ -23,7 +23,6 @@ Buffer::~Buffer() {
     }
 }
 
-
 GLuint Buffer::getType() {
     return m_buffer_type;
 }
@@ -40,7 +39,6 @@ bool Buffer::isValid() {
     return m_handle != 0;
 }
 
-
 void Buffer::setData(size_t size, void* data, GLuint access_type) {
     glBindBuffer(m_buffer_type, m_handle);
     glBufferData(m_buffer_type, size, data, access_type);
@@ -51,6 +49,66 @@ void Buffer::setData(size_t size, void* data, GLuint access_type) {
 void Buffer::preallocate(size_t size, GLuint access_type) {
     setData(size, nullptr, access_type);
 }
+
+
+ShaderStorageBuffer::ShaderStorageBuffer(const std::string& constant_name) : Buffer(GL_SHADER_STORAGE_BUFFER),
+    m_constant_name(constant_name) {
+}
+
+void ShaderStorageBuffer::bind(ShaderManager& shader_manager) {
+    if (!isValid()) {
+        return;
+    }
+    if (!m_constant_ref.isInitialized()) {
+        m_constant_ref.initialize(shader_manager, m_constant_name);
+    }
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, m_constant_ref.get(), getHandle());
+}
+
+std::string ShaderStorageBuffer::getConstantName() {
+    return m_constant_name;
+}
+
+
+Texture::Texture(int width, int height, GLuint internal_format, GLuint format, GLuint data_type, void* data) {
+    m_width = width;
+    m_height = height;
+    m_internal_format = internal_format;
+    m_format = format;
+    m_data_type = data_type;
+
+    glGenTextures(1, &m_handle);
+    glBindTexture(GL_TEXTURE_2D, m_handle);
+    glTexImage2D(GL_TEXTURE_2D, 0, m_internal_format, m_width, m_height, 0, m_format, m_data_type, data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+Texture::Texture(Texture&& other) {
+    m_handle = other.m_handle;
+    m_width = other.m_width;
+    m_height = other.m_height;
+    m_internal_format = other.m_internal_format;
+    m_format = other.m_format;
+    m_data_type = other.m_data_type;
+    other.m_handle = 0;
+}
+
+Texture::~Texture() {
+    if (m_handle != 0) {
+        glDeleteTextures(1, &m_handle);
+    }
+}
+
+GLuint Texture::getHandle() {
+    return m_handle;
+}
+
+bool Texture::isValid() {
+    return m_handle != 0;
+}
+
 
 } // opengl
 } // voxel
--- source/voxel/common/opengl.h
+++ source/voxel/common/opengl.h
@@ -1,10 +1,51 @@
 #ifndef VOXEL_ENGINE_OPENGL_H
 #define VOXEL_ENGINE_OPENGL_H
 
+#include <string>
+#include <functional>
+
 #include <glad/glad.h>
+#include "voxel/common/shaders.h"
+
 
 namespace voxel {
 namespace opengl {
+
+// IMPORTANT NOTE
+// All further entities are designed only to work inside single RenderContext and with single ShaderManager
+// passing different contexts each time will result in undefined behavior and most likely crash
+
+// Used to create opengl related entities only when render thread will require them for the first time
+template<typename T>
+class OnDemand {
+    std::function<T*()> m_factory;
+    T* m_ptr = nullptr;
+public:
+
+    OnDemand() {}
+    OnDemand(const std::function<T*()>& factory) : m_factory(factory) {
+    }
+
+    inline T& get() {
+        if (m_ptr == nullptr) {
+            m_ptr = m_factory();
+        }
+        return *m_ptr;
+    }
+
+    inline T& get(const std::function<T*()>& factory) {
+        m_factory = factory;
+        return get();
+    }
+
+    inline T* operator->() {
+        return &get();
+    }
+
+    inline T& operator*() {
+        return get();
+    }
+};
 
 // Universal class for opengl buffers
 class Buffer {
@@ -20,7 +61,7 @@ private:
 
 public:
     Buffer(GLuint buffer_type);
-    Buffer(Buffer const&) = delete;
+    Buffer(const Buffer&) = delete;
     Buffer(Buffer&&);
     ~Buffer();
 
@@ -31,6 +72,70 @@ public:
 
     void setData(size_t size, void* data, GLuint access_type);
     void preallocate(size_t size, GLuint access_type);
+};
+
+
+// represents buffer, that is bound to specified shader constant name
+// only one implementation should exist per shader manager
+class ShaderStorageBuffer : public Buffer {
+    std::string m_constant_name;
+    ShaderConstantRef<int> m_constant_ref;
+
+public:
+    ShaderStorageBuffer(const std::string& constant_name);
+    void bind(ShaderManager& shader_manager);
+    std::string getConstantName();
+};
+
+
+template<typename T>
+class ShaderUniformBuffer : public ShaderStorageBuffer {
+    T m_data;
+    T* m_data_ptr = nullptr;
+
+public:
+    ShaderUniformBuffer(const std::string& constant_name) : ShaderStorageBuffer(constant_name) {
+    }
+
+    ShaderUniformBuffer(const std::string& constant_name, const T& data): ShaderStorageBuffer(constant_name), m_data(data) {
+    }
+
+    ShaderUniformBuffer(const std::string& constant_name, T* data): ShaderStorageBuffer(constant_name), m_data_ptr(data) {
+    }
+
+    void bindUniform(ShaderManager& shader_manager) {
+        setData(sizeof(T), m_data_ptr ? m_data_ptr : &m_data, GL_STATIC_READ);
+        ShaderStorageBuffer::bind(shader_manager);
+    }
+
+    inline T* operator->() {
+        return m_data_ptr ? m_data_ptr : &m_data;
+    }
+
+    inline T& operator*() {
+        if (m_data_ptr) {
+            return *m_data_ptr;
+        } else {
+            return m_data;
+        }
+    }
+};
+
+
+class Texture {
+    GLuint m_handle;
+    int m_width, m_height;
+    GLuint m_format;
+    GLuint m_internal_format;
+    GLuint m_data_type;
+public:
+    Texture(int width, int height, GLuint internal_format, GLuint format, GLuint data_type, void* data = nullptr);
+    Texture(const Texture& other) = delete;
+    Texture(Texture&& other);
+    ~Texture();
+
+    GLuint getHandle();
+    bool isValid();
 };
 
 
